1c2242e7ee26ac4cd4194aab4e66697b4dbc2910
Wozunubi <78384472+Wozunubi@users.noreply.github.com>
2021-08-21 14:39:54 -0400

    Add files via upload

---
+++ Chess1/src/Rook.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+// Importing library for ImageIcon
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author ahhhhhhhh
+ */
+public class Rook extends Piece {
+    
+    // Constructor
+    public Rook (int x, int y, boolean isWhite, ImageIcon image) {
+        
+        // Calls parent constructor, passes in variables
+        super(x, y, isWhite, image);
+    }
+    
+    // Movement stuff or whatever yall want to put here
+}
