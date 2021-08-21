a9c3139bffe66c413e5bd74aac8799520f6120ef
JoannaHe2 <89207838+JoannaHe2@users.noreply.github.com>
2021-08-21 18:13:18 -0400

    Update Rook.java

--- Chess1/src/Rook.java
+++ Chess1/src/Rook.java
@@ -20,5 +20,61 @@ public class Rook extends Piece {
         super(x, y, isWhite, image);
     }
     
-    // Movement stuff or whatever yall want to put here
+    @Override
+    public boolean isLegalMove(Chess1 chess, int xPos, int yPos) {
+        // If the target position is not in a staight line, return false
+        if (this.x != xPos || this.y != yPos){
+            return false;
+        }	
+        
+        // Loops through the white pieces
+        for (Piece z : chess.whitePlayer){
+            
+            // If the target position already has a piece occupying it and is same colour, return false
+            if ((z.x == xPos && z.y == yPos) && 
+                (this.isWhite ^ z.isWhite) ){
+                return false;
+            }
+            
+            // If any white piece is in the middle of the vertical destination, return false
+            if ((this.x == xPos && this.y != yPos) &&
+                ((this.y < yPos) && (this.y < z.y) && (z.y < yPos)) || 
+                ((this.y > yPos) && (this.y > z.y) && (z.y > yPos))) {
+                return false;	
+            }
+            
+            // If any white piece is in the middle of the horizontal destination, return false	
+            if ((this.x != xPos && this.y == yPos) &&
+                ((this.x < xPos) && (this.x < z.x) && (z.x < xPos)) || 
+                ((this.x > xPos) && (this.x > z.x) && (z.x > xPos))) {
+                return false;	
+            } 
+        }
+        
+        // Loops through the black pieces
+        for (Piece z : chess.blackPlayer){
+            
+            // If target position already has a piece occupying it and is same colour, return false
+            if ((z.x == xPos && z.y == yPos) && 
+                (this.isWhite ^ z.isWhite) ){
+                return false;
+            }
+            
+            // If any black piece is in the middle of the vertical destination, return false
+            if ((this.x == xPos && this.y != yPos) &&
+                ((this.y < yPos) && (this.y < z.y) && (z.y < yPos)) || 
+                ((this.y > yPos) && (this.y > z.y) && (z.y > yPos))) {
+                return false;	
+            }
+            
+            // If any black piece is in the middle of the horizontal destination, return false	
+            if ((this.x != xPos && this.y == yPos) &&
+                ((this.x < xPos) && (this.x < z.x) && (z.x < xPos)) || 
+                ((this.x > xPos) && (this.x > z.x) && (z.x > xPos))) {
+                return false;	
+            } 
+        }
+		
+        return true;
+    } 
 }
