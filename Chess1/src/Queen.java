29754fbf43d0c87de2ef28d60b11722f92ad6533
Vixian12 <83770550+Vixian12@users.noreply.github.com>
2021-08-21 20:36:58 -0400

    Add files via upload

--- Chess1/src/Queen.java
+++ Chess1/src/Queen.java
@@ -1,29 +1,120 @@
 
-
 /**
  *
  * @author Maia Dinsdale
  */
-
-//importing library for image icon
+//imports ImageIcon library
 import javax.swing.ImageIcon;
-
-public class Bishop extends Piece{
+public class Queen extends Piece{
     
-    public Bishop (int x, int y, boolean isWhite, ImageIcon image){
-       //calls superclass to get variables 
-        super(x, y, isWhite, image);  
+    public Queen(int x, int y, boolean isWhite, ImageIcon image){
+        //calls superclass to get variables
+        super(x, y, isWhite, image);
     }
+    
     @Override
-    public boolean isLegalMove(Chess1 chess, int xPos, int yPos){
-      
+    public boolean isLegalMove (Chess1 chess, int xPos, int yPos){
         //distance moved horizontally
         int xdiff = x - xPos;
         //distance moved vertically
         int ydiff = y - yPos;
         
-        //checks to see if piece moved an equal x and y distance (diagonal)
-        if (Math.abs(xdiff) == Math.abs(ydiff)){
+        //checking for vertical moves
+        if (xdiff == 0){
+            //check if piece moved downwards
+            if (ydiff > 0){
+                for (int i = y; i > yPos; i--){
+                    //check colour of piece
+                    if (isWhite == true){
+                        //goes through the list of white pieces and sees if they intersect with the piece
+                        for(Piece piece : chess.whitePlayer){
+                            if (piece.y == i){
+                                return false;
+                            } 
+                        }
+                    }
+                    else{
+                        //goes through the list of black pieces and sees if they intersect with the piece
+                        for(Piece piece : chess.blackPlayer){
+                            if (piece.y == i){
+                                return false;
+                            }    
+                        }
+                    }
+                }
+            }
+            //check if piece moved upwards
+            else if (ydiff < 0){
+                for (int i = y; i < yPos; i++){
+                    //check colour of piece
+                    if (isWhite == true){
+                        //goes through the list of white pieces and sees if they intersect with the piece
+                        for(Piece piece : chess.whitePlayer){
+                            if (piece.y == i){
+                                return false;
+                            } 
+                        }
+                    }
+                    else{
+                        //goes through the list of black pieces and sees if they intersect with the piece
+                        for(Piece piece : chess.blackPlayer){
+                            if (piece.y == i){
+                                return false;
+                            }    
+                        }
+                    }
+                }
+            } 
+        }
+        //checking for horizontal moves
+        else if (ydiff == 0){
+            //check if piece moved left
+            if (xdiff > 0){
+                for (int i = x; i > xPos; i--){
+                    //check colour of piece
+                    if (isWhite == true){
+                        //goes through the list of white pieces and sees if they intersect with the piece
+                        for(Piece piece : chess.whitePlayer){
+                            if (piece.x == i){
+                                return false;
+                            } 
+                        }
+                    }
+                    else{
+                        //goes through the list of black pieces and sees if they intersect with the piece
+                        for(Piece piece : chess.blackPlayer){
+                            if (piece.x == i){
+                                return false;
+                            }    
+                        }
+                    }
+                }
+            }
+            //check if piece moved up
+            else if (ydiff < 0){
+                for (int i = x; i < xPos; i++){
+                    //check colour of piece
+                    if (isWhite == true){
+                        //goes through the list of white pieces and sees if they intersect with the piece
+                        for(Piece piece : chess.whitePlayer){
+                            if (piece.x == i){
+                                return false;
+                            } 
+                        }
+                    }
+                    else{
+                        //goes through the list of black pieces and sees if they intersect with the piece
+                        for(Piece piece : chess.blackPlayer){
+                            if (piece.x == i){
+                                return false;
+                            }    
+                        }
+                    }
+                }
+            } 
+        }
+        //checking for diagonal moves
+        else if (Math.abs(xdiff) == Math.abs(ydiff)){
             //check if piece moved left
             if (xdiff > 0){
                 for (int i = x; i >= xPos; i--){
@@ -121,9 +212,6 @@ public class Bishop extends Piece{
                 } 
             }
         }
-        else{
-            return false;
-            }
-    return true;
+        return true;
     }
 }
