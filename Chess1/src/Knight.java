2da123382549efdb161f270e110e71abb905d0a9
SashaHanson <89207379+SashaHanson@users.noreply.github.com>
2021-08-22 15:10:09 -0400

    Add files via upload

--- Chess1/src/Knight.java
+++ Chess1/src/Knight.java
@@ -23,37 +23,52 @@ public class Knight extends Piece {
     @Override
     public boolean isLegalMove(Chess1 chess, int xPos, int yPos) { 
         // If the target position is not a legal move then return false
+        
+        if ((x - xPos == 1 && y - yPos == 2) ||
+            (x - xPos == 1 && y - yPos == -2) ||
+            (x - xPos == -1 && y - yPos == 2) ||
+            (x - xPos == -1 && y - yPos == -2) ||
+            (x - xPos == 2 && y - yPos == 1) ||
+            (x - xPos == 2 && y - yPos == -1) ||
+            (x - xPos == -2 && y - yPos == 1) ||
+            (x - xPos == -2 && y - yPos == -1)) {
+            
+            // Loops through the white pieces
+            for (Piece z : chess.whitePlayer){
+                
+                // If the target position has another piece of the same colour on it, return false
+                if ((z.getX() == xPos && z.getY() == yPos) && this.isWhite ^ z.isWhite == false) {
+                    return false;
+                }
+            }
+            //****************************white can only capture balck and not white when the black loop isn't ran and vice versa***************************
+            // to fix this issue i think a counter to diferentiate whos turn it is would work becuase then only the loop for the colour whos turn it is is being run
+            /* for that reason this black loop is commented out so that white works 100%
+            // Loops through the black pieces
+            for (Piece z : chess.blackPlayer){
+                
+                // If the target position has another piece of the same colour on it, return false
+                if ((z.getX() == xPos && z.getY() == yPos) && this.isWhite ^ z.isWhite == false) {
+                    return false;
+                }
+            }*/
+            return true;
+        }
+        /*
+        
 
         // set a 2 dimesional array for all possible knight moves
         int[][] legalKnightMoves = new int[][] {{1, 2}, {1, -2}, {-1, 2}, {-1, -2}, {2, 1}, {2, -1}, {-2, 1}, {-2, -1}};
-        boolean legalKnight = false;
         
         //iterate throught the legalKnightMoves array to check if the desired move is legal
         for (int[] z : legalKnightMoves) {
             if ((x + z[0] == xPos) && (y + z[1] == yPos)) {
-                legalKnight = true;
-            }
-        }
-        
-        // Loops through the white pieces
-        for (Piece z : chess.whitePlayer){
-                
-            // If the target position has another piece of the same colour on it, return false
-            if ((z.getX() == xPos && z.getY() == yPos) && this.isWhite ^ z.isWhite == false && legalKnight == true) {
-                return false;
-            }
-        }
-        //****************************white can only capture balck and not white when the black loop isn't ran and vice versa***************************
-        // to fix this issue i think a counter to diferentiate whos turn it is would work becuase then only the loop for the colour whos turn it is is being run
-        /* for that reason this black loop is commented out so that white works 100%
-        // Loops through the black pieces
-        for (Piece z : chess.blackPlayer){
-               
-            // If the target position has another piece of the same colour on it, return false
-            if ((z.getX() == xPos && z.getY() == yPos) && this.isWhite ^ z.isWhite == false) {
-                return false;
+                return true;
             }
         }*/
         return false;
-    } 
+    }
 }
+
+
+
