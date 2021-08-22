b36bcd65bd8a265238646e30f56ed7e25685b581
SashaHanson <89207379+SashaHanson@users.noreply.github.com>
2021-08-22 12:19:10 -0400

    Add files via upload

---
+++ Chess1/src/King.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+// Importing library for ImageIcon
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author ahhhhhhhh
+ */
+public class King extends Piece {
+    
+    // Constructor
+    public King (int x, int y, boolean isWhite, ImageIcon image) {
+        
+        // Calls parent constructor, passes in variables
+        super(x, y, isWhite, image);
+    }
+    
+    @Override
+    public boolean isLegalMove(Chess1 chess, int xPos, int yPos) {
+        // If the target position is not a legal move then return false
+        
+        //could be set as an array that is iterated through but this still works
+        if ((x - xPos == -1 && y - yPos == 1) ||
+            (x - xPos == -1 && y - yPos == 0) ||
+            (x - xPos == -1 && y - yPos == -1) ||
+            (x - xPos == 0 && y - yPos == 1) ||
+            (x - xPos == 0 && y - yPos == -1) ||
+            (x - xPos == 1 && y - yPos == 1) ||
+            (x - xPos == 1 && y - yPos == 0) ||
+            (x - xPos == 1 && y - yPos == -1)) {
+            return true;
+        }
+        return false;
+    } 
+}
