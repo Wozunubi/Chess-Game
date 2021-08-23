0788f5a109b753b4b4f810934a4b9d117d8957e5
SashaHanson <89207379+SashaHanson@users.noreply.github.com>
2021-08-23 12:23:19 -0400

    Add files via upload

--- Chess1/src/whiteWins.java
+++ Chess1/src/whiteWins.java
@@ -34,7 +34,7 @@ public class whiteWins extends javax.swing.JFrame {
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/White Wins.jpg"))); // NOI18N
-        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(-90, -10, -1, -1));
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
 
         btnExit.setText("Exit");
         btnExit.addActionListener(new java.awt.event.ActionListener() {
@@ -42,7 +42,7 @@ public class whiteWins extends javax.swing.JFrame {
                 btnExitActionPerformed(evt);
             }
         });
-        getContentPane().add(btnExit, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 170, 260, 80));
+        getContentPane().add(btnExit, new org.netbeans.lib.awtextra.AbsoluteConstraints(490, 170, 260, 80));
 
         btnPlayAgain.setText("Play Again");
         btnPlayAgain.addActionListener(new java.awt.event.ActionListener() {
@@ -50,7 +50,7 @@ public class whiteWins extends javax.swing.JFrame {
                 btnPlayAgainActionPerformed(evt);
             }
         });
-        getContentPane().add(btnPlayAgain, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 170, 260, 80));
+        getContentPane().add(btnPlayAgain, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 170, 260, 80));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
