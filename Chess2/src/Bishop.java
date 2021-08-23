8b74fe6ca17d69a0d57f4598f7cf09a2a7c39e01
JoannaHe2 <89207838+JoannaHe2@users.noreply.github.com>
2021-08-23 19:21:50 -0400

    Update Bishop.java

--- Chess2/src/Bishop.java
+++ Chess2/src/Bishop.java
@@ -15,8 +15,37 @@ public class Bishop extends Piece{
     }
     @Override
     public boolean isLegalMove(Chess1 chess, int xPos, int yPos){
+        // Combines the white and black pieces together
+        ArrayList<Piece> allPlayer = new ArrayList<Piece>();
+        allPlayer.addAll(chess.whitePlayer);
+        allPlayer.addAll(chess.blackPlayer);
+	
+        // If the destination is not a diagonal, it's not a legal move
+        if (Math.abs(this.getX() - xPos) != Math.abs(this.getY() - yPos)){
+            return false;
+	    }
+	    // Loops through all the pieces	
+        for (Piece z : allPlayer){
+            // If the destination space has a piece of the same colour, it's not allowed to move there
+            if (z.getX() == xPos && 
+                z.getY() == yPos &&
+                this.getIsWhite() == z.getIsWhite()){
+                return false;
+            }
+            
+            // Checks if there are any pieces on the bishop's path
+            if (Math.abs(this.getX() - z.getX()) == Math.abs(this.getY() - z.getY())) {
+                // Checks if any pieces are blocking the bishop's path
+                if ((this.getX() < z.getX() && z.getX() < xPos)||
+                    (this.getX() > z.getX() && z.getX() > xPos)){
+                    return false;
+                }	
+            }	
+        }
+        // Otherwise it's a legal move
+        return true;
       
-        //distance moved horizontally
+        /* //distance moved horizontally
         int xdiff = x - xPos;
         //distance moved vertically
         int ydiff = y - yPos;
@@ -123,6 +152,6 @@ public class Bishop extends Piece{
         else{
             return false;
             }
-    return true;
+        return true; */
     }
 }
