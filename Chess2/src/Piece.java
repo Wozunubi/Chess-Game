96b5d7d5f56c1ee403de3cbc2287a06b9279c5c0
liya2 <89207621+liya2@users.noreply.github.com>
2021-08-23 12:40:21 -0400

    Add files via upload

---
+++ Chess2/src/Piece.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+// Importing library for ImageIcon
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author ahhhhhh
+ */
+public class Piece {
+    
+    int x, y; // Piece x and y position
+    boolean isWhite; // Piece colour
+    ImageIcon image; // Image to represent the piece
+    
+    // Constructor
+    public Piece(int x, int y, boolean isWhite, ImageIcon image) {
+        // Sets global variables to variables passed in
+        this.x = x;
+        this.y = y;
+        this.isWhite = isWhite;
+        this.image = image;
+    }
+    
+    // Returns whether the piece is black or white
+    public boolean getIsWhite() {
+        return isWhite;
+    }
+    
+    // Returns the x position
+    public int getX() {
+        return x;
+    }
+    
+    // Returns the y position
+    public int getY() {
+        return y;
+    }
+    
+    // Sets the x position
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    // Sets the y position
+    public void setY(int y) {
+        this.y = y;
+    }
+    
+    // Returns the image icon
+    public ImageIcon getImage() {
+        return image;
+    }
+    
+    // This method will be overridden in the subclasses
+    public boolean isLegalMove(Chess1 chess, int xPos, int yPos) {        
+        return true;
+    }
+}
