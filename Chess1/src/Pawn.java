7f96635a6e330e28fe1d6444307f0b57157c5f25
JoannaHe2 <89207838+JoannaHe2@users.noreply.github.com>
2021-08-22 20:35:38 -0400

    Update Pawn.java

--- Chess1/src/Pawn.java
+++ Chess1/src/Pawn.java
@@ -6,7 +6,7 @@
 
 // Importing library for ImageIcon
 import javax.swing.ImageIcon;
-
+import java.util.ArrayList;
 /**
  *
  * @author Joanna He
@@ -23,106 +23,89 @@ public class Pawn extends Piece {
     @Override
     public boolean isLegalMove(Chess1 chess, int xPos, int yPos) {
 		
-        boolean isOccupied = false; // Declares a variable to see if a position is occupied
-        boolean isBlocked = false; // Declares a variable to see if a position is being blocked on the first move
-		
-        // Loops through the white pieces
-        for (Piece z : chess.whitePlayer){
-            
-            // If the target position has another piece on it, set isOccupied to true
-            if (z.x == xPos && z.y == yPos) {
-                isOccupied = true;
-            }
-            
-            // If the target position has another piece blocking it, set isBlocked to true
-            if (this.x == xPos && 
-                this.x == z.x &&
-                this.y == 2 &&				
-                z.y == 3 &&
-                yPos == 4 ) {
-                
-                isBlocked = true;
-            }
-        }
+        boolean isBlocked = false;
+        // Combines the white and black pieces together	
+        ArrayList<Piece> allPlayer = new ArrayList<Piece>();
+        allPlayer.addAll(chess.whitePlayer);
+        allPlayer.addAll(chess.blackPlayer);
 
-        // Loops through the black pieces
-        for (Piece z : chess.blackPlayer){
-            
-            // If the target position has another piece on it, set isOccupied to true
-            if (z.x == xPos && z.y == yPos) {
-                isOccupied = true;
-            }
-            
-            // If the target position has another piece blocking it, set isBlocked to true
-            if (this.x == xPos &&
-                this.x == z.x &&
-                this.y == 7 &&
-                z.y == 6 &&
-                yPos == 5) {
-                
-                isBlocked = true;
-            }
-        }
-		
-        // Loops through the white pieces
-        for (Piece z : chess.whitePlayer) {
-            
-            // If the pawn has not made its first move yet
-            if ((this.x == xPos && this.y == 2) && 
-                (yPos == 3 || yPos == 4) && // and the target position is either 1 or 2 spaces forward
-                !isOccupied && !isBlocked) { // and the target position is not being occupied and not being blocked
-                
-                return true; // Returns legal move as true 			
-            }
-            
-            // If the pawn already made its first move
-            if (this.x == xPos && this.y > 2 &&
-                (yPos - this.y) == 1 && // and the target position is only 1 space forward
-                !isOccupied) { // and the space is not being occupied by another piece
-                
-                return true; // Returns legal move as true
-            }
-//***************************************white cant capture but black can*******************************
-//*****************************other than capturing the opposite colour the logic works*****************
-            // If the pawn wants to move forward one space diagonally
-            if (Math.abs(this.x - xPos) == 1 &&
-                (yPos - this.y) == 1 &&
-                isOccupied == true && z.isWhite == false) { // and the space is being occupied by a black piece 
-                
-                return true; // Capture the black piece
-            }	
-	}
-		
-        // Loops through the black pieces
-        for (Piece z : chess.blackPlayer) {
-            
-            // If the pawn has not made its first move yet
-            if ((this.x == xPos && this.y == 7) && 
-                (yPos == 6 || yPos == 5) && // and the target position is either 1 or 2 spaces forward
-                !isOccupied && !isBlocked) { // and the target position is not being occupied and not being blocked
-                
-                return true; // Returns legal move as true			
-            }
+        // Check if it's a white pawn
+        if (this.getIsWhite()){
+            // Loops through all the pieces
+            for (Piece z : allPlayer){
+                // Checks to see if the destination is blocked by other pieces
+                if (this.getX() == xPos &&
+                    this.getX() == z.getX() &&
+                    ((this.getY() - z.getY() == -1)||
+                    (this.getY() == 2 && z.getY() == 3 && yPos == 4)||
+                    (this.getY() == 2 && z.getY() == 4 && yPos == 4))){
+                    isBlocked = true;
+                }
+            }			
+        // Else it's a black pawn
+        } else {
+            // Loops through all the pieces
+            for (Piece z : allPlayer){
+                // Checks to see if the destination is blocked by other pieces
+                if (this.getX() == xPos &&
+                    this.getX() == z.getX() &&
+                    ((this.getY() - z.getY() == 1)||
+                    (this.getY() == 7 && z.getY() == 6 && yPos == 5)||
+                    (this.getY() == 7 && z.getY() == 5 && yPos == 5))){
+                    isBlocked = true;
+                }
+            }			
+        }		
 
-            // If the pawn already made its first move			
-            if (this.x == xPos && this.y < 7 &&
-                (this.y - yPos) == 1 && // and the target position is only 1 space forward
-                !isOccupied) { // and the space is not being occupied by another piece
-                
-                return true; // Returns legal move as true
+        // Check if it's a white pawn
+        if (this.getIsWhite()){
+            // Moves the pawn diagonally forward one space to capture the black piece 
+            for (Piece z : chess.blackPlayer){	
+                if (Math.abs(this.getX() - z.getX()) == 1 &&
+                    this.getY() - z.getY() == -1 &&
+                    z.getX() == xPos &&
+                    z.getY() == yPos) {
+                    return true;
+                }
             }
-
-            // If the pawn wants to move forward one space diagonally
-            if (Math.abs(this.x - xPos) == 1 &&
-                (this.y - yPos) == 1 &&
-                isOccupied == true && z.isWhite == true) { // and the space is being occupied by a white piece
-                
-                return true; // Capture the white piece
-            }	
-        }
-		
-	return false;	
-		
-    }
-    
+            // Moves the pawn one or two spaces during it's first move
+            if ((this.getX() == xPos && this.getY() == 2) && 
+                (yPos == 3 || yPos == 4) &&
+                !isBlocked ) {
+                return true;			
+            }
+            // Moves the pawn one space forward	
+            if (this.getX() == xPos && 
+                this.getY() >= 3 &&
+                this.getY() - yPos == -1 &&
+                !isBlocked ) {
+                return true;
+            }
+        } else {
+            // Moves the pawn diagonally forward one space to capture the white piece
+            for (Piece z : chess.whitePlayer){	
+                if (Math.abs(this.getX() - z.getX()) == 1 &&
+                    this.getY() - z.getY() == 1 &&
+                    z.getX() == xPos &&
+                    z.getY() == yPos) {
+                    return true;
+                }		
+            }
+            // Moves the pawn one or two spaces during it's first move
+            if ((this.getX() == xPos && this.getY() == 7) && 
+                (yPos == 6 || yPos == 5) &&
+                !isBlocked ) {
+                return true;			
+            }
+            // Moves the pawn one space forward	
+            if (this.getX() == xPos && 
+                this.getY() <= 6 &&
+                this.getY() - yPos == 1 &&
+                !isBlocked ) {
+                return true;
+            }
+        }	
+        // Otherwise, it's a illegal move
+        return false;		
+    } 	
 }
