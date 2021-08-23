3ecc93e557b4f091709066b2a6f3ce6271946aa0
liya2 <89207621+liya2@users.noreply.github.com>
2021-08-23 10:51:39 -0400

    Update Chess1.java

--- Rough Good Copy/Chess1/src/Chess1.java
+++ Rough Good Copy/Chess1/src/Chess1.java
@@ -1470,7 +1470,7 @@ public class Chess1 extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Chess1().setVisible(true);
+                new TitlePage().setVisible(true);
             }
         });
     }
