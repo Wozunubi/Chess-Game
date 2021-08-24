e99df27196e28424a96983312f9f979b30bef8f2
Wozunubi <78384472+Wozunubi@users.noreply.github.com>
2021-08-24 09:06:05 -0400

    Add files via upload

---
+++ Newest Version/src/src/King.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+// Importing library for ImageIcon
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author ahhhhhhhh
+ */
+public class King extends Piece {
+    
+    // Constructor
+    public King (int x, int y, boolean isWhite, ImageIcon image) {
+        
+        // Calls parent constructor, passes in variables
+        super(x, y, isWhite, image);
+    }
+    
+    @Override
+    public boolean isLegalMove(Chess chess, int xPos, int yPos) {
+        // If the target position is not a legal move then return false
+
+        // set a 2 dimesional array for all possible knight moves
+        int[][] legalKingMoves = new int[][] {{-1, 1}, {-1, 0}, {-1, -1}, {0, 1}, {0, -1}, {1, 1}, {1, 0}, {1, -1}};
+        
+        //iterate throught the legalKnightMoves array to check if the desired move is legal
+        for (int[] w : legalKingMoves) {
+            if ((x + w[0] == xPos) && (y + w[1] == yPos)) {
+                
+                // Loops through the white pieces
+                for (Piece z : chess.whitePlayer){
+                
+                    // If the target position has another piece of the same colour on it, return false
+                    if ((z.getX() == xPos && z.getY() == yPos) && this.getIsWhite()) {
+                        return false;
+                    }
+                }
+            
+                // Loops through the black pieces
+                for (Piece z : chess.blackPlayer){
+                
+                    // If the target position has another piece of the same colour on it, return false
+                    if ((z.getX() == xPos && z.getY() == yPos) && !this.getIsWhite()) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ Newest Version/src/src/Queen.java
@@ -0,0 +1,72 @@
+/**
+ *
+ * @author Maia Dinsdale
+ */
+//imports ImageIcon library
+import javax.swing.ImageIcon;
+import java.util.ArrayList;
+
+public class Queen extends Piece{
+    
+    public Queen(int x, int y, boolean isWhite, ImageIcon image){
+        //calls superclass to get variables
+        super(x, y, isWhite, image);
+    }
+    
+    @Override
+    public boolean isLegalMove (Chess chess, int xPos, int yPos){
+        // Combines the white and black pieces together
+        ArrayList<Piece> allPlayer = new ArrayList<Piece>();
+        allPlayer.addAll(chess.whitePlayer);
+        allPlayer.addAll(chess.blackPlayer);
+
+        // If the destination is not a diagonal or in a straight line, it's not a legal move
+        if (!(Math.abs(this.getX() - xPos) == Math.abs(this.getY() - yPos) ||
+            this.getX() == xPos ||
+            this.getY() == yPos)){
+            return false;
+        }
+        
+        // Loops through all the pieces
+        for (Piece z : allPlayer){
+
+            // If the destination space has a piece of the same colour, it's not allowed to move there
+            if (z.getX() == xPos && 
+                z.getY() == yPos &&
+                this.getIsWhite() == z.getIsWhite()){
+                return false;
+            }
+
+            // Checks if there is a piece on the bishop's path and if the destination space is being blocked by the piece (diagonal northeast & southwest)
+            if ((this.getX() - z.getX() == this.getY() - z.getY()) &&
+                (this.getX() - xPos == this.getY() - yPos) &&
+                ((this.getX() < z.getX() && z.getX() < xPos)||
+                (this.getX() > z.getX() && z.getX() > xPos))){
+                return false;
+            }
+
+            // Checks if there is a piece on the bishop's path and if the destination space is being blocked by the piece (diagonal northwest & southeast)
+            if ((this.getX() - z.getX() == z.getY() - this.getY()) &&
+                (this.getX() - xPos == yPos - this.getY()) &&
+                ((this.getX() < z.getX() && z.getX() < xPos)||
+                (this.getX() > z.getX() && z.getX() > xPos))){
+                return false;
+            }
+            
+            // If the destination space has a piece blocking it vertically, it's not allowed to move there
+            if (this.getX() == z.getX() && z.getX() == xPos &&
+                ((this.getY() < z.getY() && z.getY() < yPos) || 
+                (this.getY() > z.getY() && z.getY() > yPos))) {
+                return false;
+            }
+            
+            // If the destination space has a piece blocking it horizontally, it's not allowed to move there	
+            if (this.getY() == z.getY() && z.getY() == yPos &&
+                ((this.getX() < z.getX() && z.getX() < xPos) || 
+                (this.getX() > z.getX() && z.getX() > xPos))) {
+                return false;	
+            }
+        }
+        return true;
+    }
+}
